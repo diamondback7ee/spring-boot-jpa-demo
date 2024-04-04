680551e7124b98142524864b4fc779dbf1e592f1
David Saucedo <diamondback7ee@gmail.com>
2024-04-03 22:11:06 -0500

    project initialization

---
+++ src/test/java/com/diamondback7ee/jpademo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamondback7ee.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
